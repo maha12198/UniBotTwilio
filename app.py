54f066d4de1ebcb3eafef9e39995a1c58ec3409b
maha12198 <45272878+maha12198@users.noreply.github.com>
2020-05-25 01:59:01 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,22 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
-
+from flask import Flask, render_template, request
 from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
-app.config[ 'SECRET_KEY' ] = 'jsbcfsbfjefebw237u3gdbdc'
-socketio = SocketIO( app )
-
-@app.route( '/', methods=['GET','POST'])
-def hello():
-  return render_template( './Unibot.html' )
-
-def messageRecived():
-  print( 'message was received!!!' )
-
-
-@socketio.on( 'my eventes' )
-def handle_my_custom_event1( json1 ):
-  import model
-  message = json1['message']
-  answer=model.chat(message)
-  json1['answer'] = answer
-  json1['bot']='UniBot'
-  print( 'recived my event: ' + str(json1 ))
-  socketio.emit( 'my response', json1, callback=messageRecived )
-  
-
 
 @app.route("/sms", methods=['POST'])
 def sms():
+ 
     # Fetch the message
-      incoming_msg = request.values.get('Body', '').lower()
-      #message = json1['message']
-      answer=model.chat(incoming_msg)
-
       resp = MessagingResponse()
+      incoming_msg = request.values.get('Body')
+	  #message = json1['message']
+	  answer=model.chat(incoming_msg)
+	  
       resp.message(answer)
 
-      answer=model.chat(message)
-
-      return str(answer)
-
+	  return str(resp)
 
 
 if __name__ == '__main__':
-  socketio.run( app, debug = True, use_reloader=False )
+   app.run()
