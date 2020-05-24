def3b28f6914ac4016a650a3484e15e05cecca1f
maha12198 <45272878+maha12198@users.noreply.github.com>
2020-05-25 01:24:23 +0400

    Update app.py

--- app.py
+++ app.py
@@ -26,9 +26,23 @@ def handle_my_custom_event1( json1 ):
   print( 'recived my event: ' + str(json1 ))
   socketio.emit( 'my response', json1, callback=messageRecived )
   
-  answer = MessagingResponse()
-  answer.message(answer)
-  return str(json1)
+  
+
+@app.route("/sms", methods=['POST'])
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Fetch the message
+	  message = json1['message']
+	  answer=model.chat(message)
+	  json1['answer'] = answer
+	  json1['bot']='UniBot'
+	  print( 'recived my event: ' + str(json1 ))
+	  socketio.emit( 'my response', json1, callback=messageRecived )
+	  
+	  answer = MessagingResponse()
+	  answer.message(answer)
+	  return str(json1)
+
 
 if __name__ == '__main__':
   socketio.run( app, debug = True, use_reloader=False )
